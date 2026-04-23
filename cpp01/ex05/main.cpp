1f30b3e3f4a774e43b1d2363edb653d466cf54da
ddmendoza0 <ddmendozafernandez@gmail.com>
2026-04-23 07:36:30 +0200

    ex05 finished

---
+++ cpp01/ex05/main.cpp
@@ -0,0 +1,15 @@
+#include "Harl.hpp"
+#include <iostream>
+
+int main( void )
+{
+    Harl    harl;
+    std::string level_name[5] = { "DEBUG", "INFO", "WARNING", "ERROR", "THIS SHOULD FAIL" };
+
+    for (int i = 0; i < 5; i++)
+    {
+        std::cout << level_name[i] << " :\n";
+        harl.complain( level_name[i] );
+    }
+    return (1);
+}
