67e848f03ed4085e60f5028edc5ae0dbe9c89098
David Mendoza Fernandez <dmendoza@car5s4.42barcelona.com>
2026-05-01 12:26:49 +0200

    cpp02 - ex03

--- cpp02/ex03/Point.hpp
+++ cpp02/ex03/Point.hpp
@@ -1,7 +1,23 @@
 #ifndef POINT_HPP
 # define POINT_HPP
 
+# include "Fixed.hpp"
 
+class	Point
+{
+	private:
+		const Fixed	_x;
+		const Fixed	_y;
+		Point& operator=(const Point& other);
+	
+	public:
+		Point();
+		Point(const float x, const float y);
+		Point(const Point& other);
+		~Point();
 
+};
+
+bool bsp( Point const a, Point const b, Point const c, Point const point);
 
 #endif
