23ea803b8fc98e0c17c28c9fbcd3d66322681a63
ddmendoza0 <ddmendozafernandez@gmail.com>
2026-05-07 06:37:48 +0200

    cpp04 - ex00 - Animal class

---
+++ cpp04/ex00/Animal.cpp
@@ -0,0 +1,32 @@
+#include "Animal.hpp"
+#include <iostream>
+
+Animal::Animal( void ) : type("Animal") 
+{
+    std::cout << "Animal default constructor called\n";
+};
+
+Animal::Animal( const Animal& other ) : type(other.type) 
+{
+    std::cout << "Animal copy constructor called\n";
+};
+
+Animal& Animal::operator=( const Animal& other )
+{
+    std::cout << "Animal assigment operator called\n";
+    if (this != &other)
+        type = other.type;
+    return (*this);
+}
+
+Animal::~Animal( void ) 
+{
+    std::cout << "Animal destructor called\n";
+}
+
+std::string Animal::getType( void ) const { return ( type ); }
+
+void Animal::makeSound( void ) const
+{
+    std::cout << "*muffled sound...*\n";
+}
---
+++ cpp04/ex00/Animal.hpp
@@ -0,0 +1,23 @@
+#ifndef ANIMAL_HPP
+# define ANIMAL_HPP
+
+#include <string>
+
+class   Animal
+{
+    protected:
+        std::string type;
+
+    public:
+        Animal(void);
+        Animal(const Animal& other);
+        Animal& operator=(const Animal& other);
+        virtual ~Animal();
+
+        std::string getType(void) const;
+
+        virtual void    makeSound(void) const;
+
+};
+
+#endif
