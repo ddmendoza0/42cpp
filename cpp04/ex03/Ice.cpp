3b3517806c0c0229c7cf8109f2cc286570e05921
ddmendoza0 <ddmendozafernandez@gmail.com>
2026-05-15 10:32:02 +0200

    cpp04 - ex03 Ice and Cure class + character interface

--- cpp04/ex03/Ice.cpp
+++ cpp04/ex03/Ice.cpp
@@ -1,3 +1,26 @@
 #include "Ice.hpp"
+#include "ICharacter.hpp"
+#include <iostream>
 
-Ice::Ice( void ) : AMateria("ice") {}
+Ice::Ice(void) : AMateria("ice") {}
+
+Ice::Ice(const Ice& other) : AMateria(other) {}
+
+Ice& Ice::operator=(const Ice& other)
+{
+    if (this != &other)
+        AMateria::operator=(other);
+    return (*this);
+}
+
+Ice::~Ice(void) {}
+
+Ice* Ice::clone() const
+{
+    return new Ice(*this);
+}
+
+void Ice::use(ICharacter& target)
+{
+    std::cout << "* shoots an ice bolt at " << target.getName() << " *" << std::endl;
+}
