bfdd7c7859fa2b7c97fbbb9d3b74d1cdbd9e4b67
ddmendoza0 <ddmendozafernandez@gmail.com>
2026-05-18 18:50:30 +0200

    cpp05 - ex00

--- cpp05/ex00/Bureaucrat.cpp
+++ cpp05/ex00/Bureaucrat.cpp
@@ -12,9 +12,9 @@ Bureaucrat::Bureaucrat( const Bureaucrat& other ) : _name(other._name), _grade(o
 
 Bureaucrat::~Bureaucrat( void ) {}
 
-const std::string& Bureaucrat::getName( void ) { return ( _name ); }
+const std::string& Bureaucrat::getName( void ) const { return ( _name ); }
 
-const std::string& Bureaucrat::getGrade( void ) { return ( _grade ); }
+int Bureaucrat::getGrade( void ) const { return ( _grade ); }
 
 void Bureaucrat::incrementGrade( void )
 {
@@ -25,7 +25,23 @@ void Bureaucrat::incrementGrade( void )
 
 void Bureaucrat::decrementGrade( void )
 {
-    if ((grade + 1) > 150)
+    if ((_grade + 1) > 150)
         throw Bureaucrat::GradeTooLowException( );
-    grade++;
+    _grade++;
+}
+
+const char* Bureaucrat::GradeTooHighException::what() const throw()
+{
+    return ( "Grade is too high" );
+}
+
+const char* Bureaucrat::GradeTooLowException::what() const throw()
+{
+    return ( "Grade is too low" );
+}
+
+std::ostream& operator<<(std::ostream& out, const Bureaucrat& b)
+{
+    out << b.getName() << ", bureaucrat grade " << b.getGrade();
+    return ( out );
 }
--- cpp05/ex00/Bureaucrat.hpp
+++ cpp05/ex00/Bureaucrat.hpp
@@ -18,7 +18,7 @@ class   Bureaucrat
         Bureaucrat(const Bureaucrat& other);
         ~Bureaucrat(void);
 
-        const std::string getName(void) const;
+        const std::string& getName(void) const;
         int getGrade(void) const;
 
         void incrementGrade(void);
