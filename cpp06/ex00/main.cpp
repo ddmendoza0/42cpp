d31757737cb95b26bb1ec9171ccc582a0f4f84db
David Mendoza <ddmendozafernandez@gmail.com>
2026-06-16 07:11:36 +0200

    cpp06 - ex00

--- cpp06/ex00/main.cpp
+++ cpp06/ex00/main.cpp
@@ -2,13 +2,14 @@
 
 int main( int argc, char **argv )
 {
-    std::string input;
-
-    if (argc != 2 || !argv[1])
-        return (1);
-
-    input = argv[1];
+    //std::string input;
+    //if (argc != 2 || !argv[1])
+        //return (1);
+    //input = argv[1];
     
+    (void)argc;
+    (void)argv;
+
     std::cout << "--- char literals ---" << std::endl;
     ScalarConverter::convert("'a'");
     ScalarConverter::convert("'Z'");
