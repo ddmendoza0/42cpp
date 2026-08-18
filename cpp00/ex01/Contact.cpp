5a589926a81b8739006b088e077756d44046ba10
David Mendoza <ddmendozafernandez@gmail.com>
2026-08-18 14:57:18 +0200

    cpp00 - ex01 - repairs: tabs and empty inputs

--- cpp00/ex01/Contact.cpp
+++ cpp00/ex01/Contact.cpp
@@ -10,6 +10,31 @@ std::string Contact::getNickName() const { return _NickName; }
 std::string Contact::getPhoneNum() const { return _PhoneNum; }
 std::string Contact::getSecret() const {return _Secret; }
 
+static std::string sanitize(const std::string &str)
+{
+    std::string clean;
+    clean.reserve(str.length());
+    for (size_t i = 0; i < str.length(); i++)
+    {
+        unsigned char c = str[i];
+        if (std::iscntrl(c))
+            clean += ' ';
+        else
+            clean += c;
+    }
+    return ( clean );
+}
+
+static bool isBlank(const std::string &str)
+{
+    for (size_t i = 0; i < str.length(); i++)
+    {
+        if (!std::isspace(static_cast<unsigned char>(str[i])))
+            return false;
+    }
+    return true;
+}
+
 bool Contact::setContact()
 {
     _FirstName = "";
@@ -22,6 +47,9 @@ bool Contact::setContact()
         std::cout << "Introduce First Name:\n";
         if (!std::getline(std::cin, _FirstName))
             return false;
+        _FirstName = sanitize(_FirstName);
+        if (isBlank(_FirstName))
+            _FirstName = "";
     }
 
     while (_LastName.empty())
@@ -29,6 +57,9 @@ bool Contact::setContact()
         std::cout << "Introduce Last Name:\n";
         if (!std::getline(std::cin, _LastName))
             return false;
+        _LastName = sanitize(_LastName);
+        if (isBlank(_LastName))
+            _LastName = "";
     }
 
     while (_NickName.empty())
@@ -36,6 +67,9 @@ bool Contact::setContact()
         std::cout << "Introduce Nick Name:\n";
         if (!std::getline(std::cin, _NickName))
             return false;
+        _NickName = sanitize(_NickName);
+        if (isBlank(_NickName))
+            _NickName = "";
     }
 
     while (_PhoneNum.empty())
@@ -43,6 +77,7 @@ bool Contact::setContact()
         std::cout << "Introduce Phone Number:\n";
         if (!std::getline(std::cin, _PhoneNum))
             return false;
+        _PhoneNum = sanitize(_PhoneNum);
         for (int i = 0; _PhoneNum[i]; i++)
         {
             if (i == 0 && _PhoneNum[0] == '+' && _PhoneNum[i + 1])
@@ -62,6 +97,7 @@ bool Contact::setContact()
         std::cout << "And your darkest secret:\n";
         if (!std::getline(std::cin, _Secret))
             return false;
+        _Secret = sanitize(_Secret);
     }
     return true;
 }
